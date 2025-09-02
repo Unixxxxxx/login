9bf825b26609b1df67ad0457372944dc1461f4ef
Sudhanshu Sharma <138148726+Unixxxxxx@users.noreply.github.com>
2025-09-02 13:11:36 +0000

    update

--- login/urls.py
+++ login/urls.py
@@ -2,12 +2,19 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from . import views
+from login import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name ='index'),
-    path('', include('myapp.urls')),
-    path('', include('myapp1.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', views.index, name='index'),       # Homepage
+    path('myapp/', include('myapp.urls')),     # App 1
+    path('myapp1/', include('myapp1.urls')),   # App 2
+]   
 
+# Serve static & media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# Custom error handlers
+handler404 = 'login.views.custom_page_not_found'
--- login/views.py
+++ login/views.py
@@ -10,3 +10,7 @@ def secure_view(request):
     if not request.user.is_superuser:
         return render(request, '403.html', status=403)
     return render(request, 'login.html')
+
+def custom_page_not_found(request, exception):
+    return render(request, "404.html", status=404)
+
